07fa5dffe52bdc682173044cac5c8008ad0816eb
Shraddha <shraddha.amrutiya@vindaloosofttech.com>
2025-03-10 11:49:07 +0530

    remove extra code

--- dist/modelse/db.modeles.js
+++ dist/modelse/db.modeles.js
@@ -1,19 +1,9 @@
 "use strict";
-// import { Pool } from "pg";
-// import dotenv from "dotenv";
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.pool = void 0;
-// dotenv.config();
-// export const pool = new Pool({
-//   user: process.env.DB_USER,
-//   host: process.env.DB_HOST,
-//   database: process.env.DB_NAME,
-//   password: process.env.DB_PASS,
-//   port: Number(process.env.DB_PORT),
-// });
 const pg_1 = require("pg");
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
